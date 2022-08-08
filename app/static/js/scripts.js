e1553799acdc9458aa59a72a0b7ec571048dda43
Liz Wang <99926165+lizz27@users.noreply.github.com>
2022-08-08 11:22:56 -0400

    Add files via upload

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -1,80 +1,105 @@
 /*!
-* Start Bootstrap - Stylish Portfolio v6.0.4 (https://startbootstrap.com/theme/stylish-portfolio)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-stylish-portfolio/blob/master/LICENSE)
+* Start Bootstrap - Grayscale v7.0.5 (https://startbootstrap.com/theme/grayscale)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
 */
+//
+// Scripts
+// 
+
 window.addEventListener('DOMContentLoaded', event => {
 
-    const sidebarWrapper = document.getElementById('sidebar-wrapper');
-    let scrollToTopVisible = false;
-    // Closes the sidebar menu
-    const menuToggle = document.body.querySelector('.menu-toggle');
-    menuToggle.addEventListener('click', event => {
-        event.preventDefault();
-        sidebarWrapper.classList.toggle('active');
-        _toggleMenuIcon();
-        menuToggle.classList.toggle('active');
-    })
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
 
-    // Closes responsive menu when a scroll trigger link is clicked
-    var scrollTriggerList = [].slice.call(document.querySelectorAll('#sidebar-wrapper .js-scroll-trigger'));
-    scrollTriggerList.map(scrollTrigger => {
-        scrollTrigger.addEventListener('click', () => {
-            sidebarWrapper.classList.remove('active');
-            menuToggle.classList.remove('active');
-            _toggleMenuIcon();
-        })
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
     });
 
-    function _toggleMenuIcon() {
-        const menuToggleBars = document.body.querySelector('.menu-toggle > .fa-bars');
-        const menuToggleTimes = document.body.querySelector('.menu-toggle > .fa-times');
-        if (menuToggleBars) {
-            menuToggleBars.classList.remove('fa-bars');
-            menuToggleBars.classList.add('fa-times');
-        }
-        if (menuToggleTimes) {
-            menuToggleTimes.classList.remove('fa-times');
-            menuToggleTimes.classList.add('fa-bars');
-        }
-    }
+});
 
-    // Scroll to top button appear
-    document.addEventListener('scroll', () => {
-        const scrollToTop = document.body.querySelector('.scroll-to-top');
-        if (document.documentElement.scrollTop > 100) {
-            if (!scrollToTopVisible) {
-                fadeIn(scrollToTop);
-                scrollToTopVisible = true;
-            }
-        } else {
-            if (scrollToTopVisible) {
-                fadeOut(scrollToTop);
-                scrollToTopVisible = false;
-            }
-        }
-    })
-})
+// Reverse Color Elements When Websites Scrolls //
+var body = document.getElementsByTagName('body')[0];
+        body.style.backgroundColor = 'white';
+var bg_changes1 = document.getElementsByClassName('bg-black')[0];
+var bg_changes2 = document.getElementsByClassName('bg-black')[1];
+var bg_changes3 = document.getElementsByClassName('bg-black')[2];
+var bg_changes4 = document.getElementsByClassName('bg-black')[3];
+var bg_changes5 = document.getElementsByClassName('bg-black')[4];
+var bg_changes6 = document.getElementsByClassName('bg-black')[5];
+var bg_changes7 = document.getElementsByClassName('bg-black')[6];
+        bg_changes1.style.backgroundColor = 'black';
+        bg_changes2.style.backgroundColor = 'black';
+        bg_changes3.style.backgroundColor = 'black';
+        bg_changes4.style.backgroundColor = 'black';
+        bg_changes5.style.backgroundColor = 'black';
+        bg_changes6.style.backgroundColor = 'black';
+        bg_changes7.style.backgroundColor = 'black';
 
-function fadeOut(el) {
-    el.style.opacity = 1;
-    (function fade() {
-        if ((el.style.opacity -= .1) < 0) {
-            el.style.display = "none";
-        } else {
-            requestAnimationFrame(fade);
-        }
-    })();
-};
+        // trigger this function every time the user scrolls
+        window.onscroll = function (event) {
+            var scroll = window.pageYOffset;
+            if (scroll < 1250) {
+                body.style.backgroundColor = 'white';
+                bg_changes1.style.backgroundColor='black';
+                bg_changes2.style.backgroundColor='black';
+                bg_changes3.style.backgroundColor='black';
+                bg_changes4.style.backgroundColor='black';
+                bg_changes5.style.backgroundColor='black';
+                bg_changes6.style.backgroundColor='black';
+                bg_changes7.style.backgroundColor='black';
+                document.getElementById('whitefont_transition_h4').style.color = 'black';
+                document.getElementById('whitefont_transition_p').style.color = 'black';
+                document.getElementById('whitefont_transition_h1').style.color = 'black';
+                document.getElementById('whitefont_transition_h2').style.color = 'black';
+            } else {
+                body.style.backgroundColor = 'black';
+                bg_changes1.style.backgroundColor='white';
+                bg_changes2.style.backgroundColor='white';
+                bg_changes3.style.backgroundColor='white';
+                bg_changes4.style.backgroundColor='white';
+                bg_changes5.style.backgroundColor='white';
+                bg_changes6.style.backgroundColor='white';
+                bg_changes7.style.backgroundColor='white';
 
-function fadeIn(el, display) {
-    el.style.opacity = 0;
-    el.style.display = display || "block";
-    (function fade() {
-        var val = parseFloat(el.style.opacity);
-        if (!((val += .1) > 1)) {
-            el.style.opacity = val;
-            requestAnimationFrame(fade);
-        }
-    })();
-};
+                document.getElementById('whitefont_transition_h4').style.color = 'white';
+                document.getElementById('whitefont_transition_p').style.color = 'white';
+                document.getElementById('whitefont_transition_h1').style.color = 'white';
+                document.getElementById('whitefont_transition_h2').style.color = 'white';
+
+            } }
